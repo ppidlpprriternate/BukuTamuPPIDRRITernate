4a78a99342b87e26c48be96378f6253f46255be7
ppidlpprriternate <ppidlpprriternate@gmail.com>
2025-11-05 08:11:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,33 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+const canvas = document.getElementById("signature-pad");
+const ctx = canvas.getContext("2d");
+let drawing = false;
+
+canvas.addEventListener("mousedown", () => (drawing = true));
+canvas.addEventListener("mouseup", () => (drawing = false, ctx.beginPath()));
+canvas.addEventListener("mousemove", draw);
+
+function draw(e) {
+  if (!drawing) return;
+  ctx.lineWidth = 2;
+  ctx.lineCap = "round";
+  ctx.strokeStyle = "#000";
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(e.offsetX, e.offsetY);
+}
+
+document.getElementById("clear-ttd").addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+document.getElementById("formBukuTamu").addEventListener("submit", function(e) {
+  const ttdData = canvas.toDataURL(); 
+  document.getElementById("ttdData").value = ttdData;
+});
 
   jenisLayanan.addEventListener("change", function () {
     terjadwalPNBP.classList.add("hidden");
@@ -60,3 +87,4 @@ document.addEventListener("DOMContentLoaded", function () {
       });
   });
 });
+
